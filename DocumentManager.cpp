99e02ba8df093edce63b328b17f5092264871c20
ducnamkhanhle <ducnamkhanh.le@student.adelaide.edu.au>
2024-05-22 17:08:48 +0930

    first commit

---
+++ DocumentManager.cpp
@@ -0,0 +1,47 @@
+#include "DocumentManager.h"
+
+void DocumentManager::addDocument(std::string name, int id, int license_limit){
+    docList[id] = std::make_tuple(name, license_limit, 0);
+}
+
+void DocumentManager::addPatron(int patronID){
+    patronList.push_back(patronID);
+}
+
+int DocumentManager::search(std::string name){
+    for (const auto &it:docList){
+        if (std::get<0>(it.second) == name){
+            return it.first;
+        }
+    }
+    return 0;
+}
+
+
+bool DocumentManager::borrowDocument(int docid, int patronID){
+    auto &it = docList[docid];
+    auto &numOfCopies = std::get<2>(it);
+    auto findPatron_it = std::find(patronList.begin(), patronList.end(), patronID);
+
+    if (std::get<0>(it) == "") {
+        return false;
+    }
+
+    if (findPatron_it == patronList.end()){
+        return false;
+    }
+
+    if (numOfCopies == std::get<1>(it)){
+        return false;
+    }
+
+    numOfCopies += 1;
+    return true;
+}
+
+void DocumentManager::returnDocument(int docid, int patronID){
+    auto &it = docList[docid];
+    auto &numOfCopies = std::get<2>(it);
+
+    numOfCopies -= 1;
+}
---
+++ DocumentManager.h
@@ -0,0 +1,22 @@
+#ifndef DOCUMENTMANAGER
+#define DOCUMENTMANAGER
+
+#include<string>
+#include<unordered_map>
+#include<tuple>
+
+
+class DocumentManager{
+    private:
+        std::unordered_map<int, std::tuple<std::string,int, int>> docList;
+        std::vector<int> patronList;
+    public:
+        DocumentManager();
+        void addDocument(std::string name, int id, int license_limit);
+        void addPatron(int patronID);
+        int search(std::string name);
+        bool borrowDocument(int docid, int patronID);  
+        void returnDocument(int docid, int patronID);
+};
+
+#endif
